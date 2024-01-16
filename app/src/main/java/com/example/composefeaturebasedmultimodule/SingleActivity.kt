f3fa5ab9169ad532e2ad68925aefa58912589cbf
suleyman.basaranoglu <sbasaranoglu63@gmail.com>
2024-01-16 14:34:49 +0300

    Add App Navigation Compose function on Single Compose Activity

---
+++ app/src/main/java/com/example/composefeaturebasedmultimodule/SingleActivity.kt
@@ -0,0 +1,40 @@
+package com.example.composefeaturebasedmultimodule
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.composefeaturebasedmultimodule.ui.theme.ComposeFeatureBasedMultiModuleTheme
+import com.example.detail.presentation.DetailScreen
+import com.example.home.presentation.HomeScreen
+import com.example.list.presentation.ListScreen
+import com.example.navigation.AppNavigation
+import com.example.navigation.Navigator
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class SingleActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var navigator: Navigator
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeFeatureBasedMultiModuleTheme {
+                AppNavigation(
+                    navigator = navigator,
+                    homeScreen = {
+                        HomeScreen()
+                    },
+                    listScreen = {
+                        ListScreen()
+                    },
+                    detailScreen = {// We can get args with "it" if we need
+                        DetailScreen()
+                    }
+                )
+            }
+        }
+    }
+}
