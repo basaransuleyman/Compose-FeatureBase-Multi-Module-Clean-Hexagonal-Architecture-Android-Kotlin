2ac77fe5bca25be92fc9bd50570079b7ef382ff5
suleyman.basaranoglu <sbasaranoglu63@gmail.com>
2024-01-16 14:35:36 +0300

    Add Main State and Event handler View Model for base pattern

---
+++ core/src/main/java/com/example/core/presentation/StateAndEventViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.core.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+abstract class StateAndEventViewModel<UiState, Event>(initialState: UiState) : ViewModel() {
+
+    private val events = MutableSharedFlow<Event>(replay = 0)
+    private val _uiState: MutableStateFlow<UiState> = MutableStateFlow(initialState)
+    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            events.collect { event ->
+                handleEvent(event)
+            }
+        }
+    }
+
+    protected abstract suspend fun handleEvent(event: Event)
+
+    protected fun updateUiState(update: UiState.() -> UiState) {
+        _uiState.value = _uiState.value.update()
+    }
+
+    fun onEvent(event: Event) {
+        viewModelScope.launch {
+            events.emit(event)
+        }
+    }
+}
