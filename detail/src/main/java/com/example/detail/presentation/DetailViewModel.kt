f44b9718a5a92d07ce4d4e86e238002c8d25e8b2
suleyman.basaranoglu <sbasaranoglu63@gmail.com>
2024-01-16 14:46:43 +0300

    Create Detail View Model

---
+++ detail/src/main/java/com/example/detail/presentation/DetailViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.detail.presentation
+
+import androidx.lifecycle.viewModelScope
+import com.example.core.presentation.StateAndEventViewModel
+import com.example.detail.domain.usecase.GetItemDetailUseCase
+import com.example.detail.presentation.state.DetailUIState
+import com.example.detail.presentation.uievent.DetailUIEvent
+import com.example.navigation.Navigator
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val getItemDetail: GetItemDetailUseCase,
+    private val navigator: Navigator,
+) : StateAndEventViewModel<DetailUIState, DetailUIEvent>(DetailUIState(null)) {
+
+    fun loadItemDetail() {
+        viewModelScope.launch {
+            getItemDetail.getDetail()
+                .onStart {
+                    updateUiState { copy(isLoading = true) }
+                }
+                .catch { exception ->
+                    updateUiState { copy(error = exception) }
+                }
+                .collect {
+                    updateUiState { copy(itemData = it, isLoading = false) }
+                }
+        }
+    }
+
+    private fun handleBack() {
+        navigator.back()
+    }
+
+    override suspend fun handleEvent(event: DetailUIEvent) {
+        when (event) {
+            is DetailUIEvent.Dismiss -> handleBack()
+            is DetailUIEvent.LoadItemDetail -> loadItemDetail()
+            else -> Unit
+        }
+    }
+
+}
