9fdd70bd4072ebdbcf7a6f8d3b27d945c3a50c8d
suleyman.basaranoglu <sbasaranoglu63@gmail.com>
2024-01-16 14:48:42 +0300

    Add Application Hilt

---
+++ app/src/main/java/com/example/composefeaturebasedmultimodule/MainApplication.kt
@@ -0,0 +1,8 @@
+package com.example.composefeaturebasedmultimodule
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication: Application() {
+}
