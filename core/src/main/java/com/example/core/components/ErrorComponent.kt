7cf7058b983357f10b3ed7c1c2047f6e641d1095
suleyman.basaranoglu <sbasaranoglu63@gmail.com>
2024-01-16 22:50:41 +0300

    Add ErrorComponent and LoadingComponent

--- core/src/main/java/com/example/core/components/ErrorComponent.kt
+++ core/src/main/java/com/example/core/components/ErrorComponent.kt
@@ -1,7 +1,19 @@
 package com.example.core.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
-fun ErrorComponent(error: Throwable?){
+fun ErrorComponent(error: Throwable?) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        error?.message?.let { Text(text = it, color = Color.Red) }
+    }
 }
--- core/src/main/java/com/example/core/components/LoadingComponent.kt
+++ core/src/main/java/com/example/core/components/LoadingComponent.kt
@@ -1,7 +1,18 @@
 package com.example.core.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun LoadingComponent() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator()
+    }
 }
