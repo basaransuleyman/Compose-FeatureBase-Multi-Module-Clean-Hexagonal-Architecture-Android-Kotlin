8e2d3f46b6eed0e8a691d22157a1a7564e489645
suleyman.basaranoglu <sbasaranoglu63@gmail.com>
2024-01-16 19:00:32 +0300

    Convert Detail Module Bottom sheet to Detail Screen

--- detail/src/main/java/com/example/detail/presentation/DetailScreen.kt
+++ detail/src/main/java/com/example/detail/presentation/DetailScreen.kt
@@ -1,7 +1,5 @@
 package com.example.detail.presentation
 
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -12,12 +10,10 @@ import com.example.core.components.LoadingComponent
 import com.example.detail.presentation.components.DetailContent
 import com.example.detail.presentation.uievent.DetailUIEvent
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailScreen() {
     val viewModel: DetailViewModel = hiltViewModel()
     val state by viewModel.uiState.collectAsState()
-    val bottomSheetState = rememberModalBottomSheetState()
 
     LaunchedEffect(Unit) {
         viewModel.onEvent(DetailUIEvent.LoadItemDetail)
@@ -28,9 +24,7 @@ fun DetailScreen() {
         state.error != null -> { ErrorComponent(error = state.error) }
         state.itemData != null -> {
             DetailContent(
-                state.itemData!!,
-                sheetState = bottomSheetState,
-                onEvent = viewModel::onEvent
+                state.itemData!!
             )
         }
     }
