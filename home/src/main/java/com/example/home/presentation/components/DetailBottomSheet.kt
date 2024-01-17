7f8244f5c411843a10d415b6b235916bea889d1d
suleyman.basaranoglu <sbasaranoglu63@gmail.com>
2024-01-17 14:50:55 +0300

    Add spacer on detail bottom sheet

--- home/src/main/java/com/example/home/presentation/components/DetailBottomSheet.kt
+++ home/src/main/java/com/example/home/presentation/components/DetailBottomSheet.kt
@@ -1,13 +1,18 @@
 package com.example.home.presentation.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.example.core.components.CoilImageComponent
 import com.example.home.domain.model.ProductItem
@@ -16,15 +21,40 @@ import com.example.home.domain.model.ProductItem
 fun DetailBottomSheet(
     productItem: ProductItem
 ) {
-    Column(modifier = Modifier.padding(16.dp)) {
-        CoilImageComponent(
-            imageUrl = productItem.productImage,
-            contentDescription = "Bottom Sheet Image",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(300.dp)
-        )
-        productItem.text?.let { Text(text = it, style = MaterialTheme.typography.bodyLarge) }
-        productItem.subText?.let { Text(text = it, style = MaterialTheme.typography.bodyMedium) }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CoilImageComponent(
+                imageUrl = productItem.productImage,
+                contentDescription = "Bottom Sheet Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            productItem.text?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.headlineLarge,
+                    color = MaterialTheme.colorScheme.secondaryContainer
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            productItem.subText?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f),
+                )
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+        }
     }
 }
